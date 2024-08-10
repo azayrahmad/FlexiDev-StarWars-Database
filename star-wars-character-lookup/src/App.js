ce46c5238df30d751e7c1ef547efe0bbc1c41d2b
Aziz Rahmad <azayrahmad@gmail.com>
2024-08-11 05:21:05 +0800

    initial SWAPI

--- star-wars-character-lookup/src/App.js
+++ star-wars-character-lookup/src/App.js
@@ -1,23 +1,38 @@
 import logo from "./logo.svg";
+import React, { useState } from "react";
+import CharacterList from "./components/CharacterList";
+import CharacterDetail from "./components/CharacterDetail";
 import "./App.css";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("list");
+  const [selectedCharacterId, setSelectedCharacterId] = useState(null);
+
+  const navigateToCharacter = (id) => {
+    setSelectedCharacterId(id);
+    setCurrentPage("detail");
+  };
+
+  const navigateToList = () => {
+    setCurrentPage("list");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Star Wars Character Lookup</h1>
       </header>
+      <main>
+        {currentPage === "list" ? (
+          <CharacterList onCharacterSelect={navigateToCharacter} />
+        ) : (
+          <CharacterDetail
+            characterId={selectedCharacterId}
+            onBackClick={navigateToList}
+          />
+        )}
+      </main>
     </div>
   );
 }
