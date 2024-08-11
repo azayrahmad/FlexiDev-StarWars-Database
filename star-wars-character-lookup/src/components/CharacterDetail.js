efbf2842b4fca08b2cad39baac62cfe567c1fafd
Aziz Rahmad <azayrahmad@gmail.com>
2024-08-11 10:38:31 +0800

    refactor character details

--- star-wars-character-lookup/src/components/CharacterDetail.js
+++ star-wars-character-lookup/src/components/CharacterDetail.js
@@ -63,33 +63,49 @@ const CharacterDetail = ({ characterUrl, onBackClick }) => {
       <p>Height: {character.height} cm</p>
       <p>Mass: {character.mass} kg</p>
 
-      {additionalInfo.films.length > 0 && <h3>Films</h3>}
-      <ul>
-        {additionalInfo.films.map((film, index) => (
-          <li key={index}>{film.title}</li>
-        ))}
-      </ul>
+      {additionalInfo.films.length > 0 && (
+        <>
+          <h3>Films</h3>
+          <ul>
+            {additionalInfo.films.map((film, index) => (
+              <li key={index}>{film.title}</li>
+            ))}
+          </ul>
+        </>
+      )}
 
-      {additionalInfo.species.length > 0 && <h3>Species</h3>}
-      <ul>
-        {additionalInfo.species.map((species, index) => (
-          <li key={index}>{species.name}</li>
-        ))}
-      </ul>
+      {additionalInfo.species.length > 0 && (
+        <>
+          <h3>Species</h3>
+          <ul>
+            {additionalInfo.species.map((species, index) => (
+              <li key={index}>{species.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
 
-      {additionalInfo.starships.length > 0 && <h3>Starships</h3>}
-      <ul>
-        {additionalInfo.starships.map((starship, index) => (
-          <li key={index}>{starship.name}</li>
-        ))}
-      </ul>
+      {additionalInfo.starships.length > 0 && (
+        <>
+          <h3>Starships</h3>
+          <ul>
+            {additionalInfo.starships.map((starship, index) => (
+              <li key={index}>{starship.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
 
-      {additionalInfo.vehicles.length > 0 && <h3>Vehicles</h3>}
-      <ul>
-        {additionalInfo.vehicles.map((vehicle, index) => (
-          <li key={index}>{vehicle.name}</li>
-        ))}
-      </ul>
+      {additionalInfo.vehicles.length > 0 && (
+        <>
+          <h3>Vehicles</h3>
+          <ul>
+            {additionalInfo.vehicles.map((vehicle, index) => (
+              <li key={index}>{vehicle.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
