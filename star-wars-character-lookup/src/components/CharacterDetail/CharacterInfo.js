013acbf99d7a2b1fca6538c7341b0528a30accf4
Aziz Rahmad <azayrahmad@gmail.com>
2024-08-13 11:52:49 +0800

    Add more character info and formatting

--- star-wars-character-lookup/src/components/CharacterDetail/CharacterInfo.js
+++ star-wars-character-lookup/src/components/CharacterDetail/CharacterInfo.js
@@ -1,19 +1,46 @@
 import React from "react";
+import useFetch from "../../hooks/useFetch";
 
 const CharacterInfo = ({ character }) => {
+  const { data: homeworldData, loading, error } = useFetch(character.homeworld);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+  if (!homeworldData) return <div>Homeworld not found</div>;
+
   return (
     <div>
       <h2>{character.name}</h2>
-      <p>Birth Year: {character.birth_year}</p>
-      <p>Gender: {character.gender}</p>
-      <p>Height: {character.height} cm</p>
-      <p>Mass: {character.mass} kg</p>
-      <p>Hair Color: {character.hair_color}</p>
-      <p>Skin Color: {character.skin_color}</p>
-      <p>Eye Color: {character.eye_color}</p>
-      {/* <p>Homeworld: {character.homeworld}</p>
-      <p>Created: {character.created}</p>
-      <p>Edited: {character.edited}</p> */}
+      <p>
+        <small>
+          <b>Created:</b> {new Date(character.created).toDateString()},{" "}
+          <b>Edited:</b> {new Date(character.edited).toDateString()}
+        </small>
+      </p>
+      <p>
+        <b>Birth Year:</b> {character.birth_year}
+      </p>
+      <p>
+        <b>Gender:</b> {character.gender}
+      </p>
+      <p>
+        <b>Height:</b> {character.height} cm
+      </p>
+      <p>
+        <b>Mass:</b> {character.mass} kg
+      </p>
+      <p>
+        <b>Hair Color:</b> {character.hair_color}
+      </p>
+      <p>
+        <b>Skin Color:</b> {character.skin_color}
+      </p>
+      <p>
+        <b>Eye Color:</b> {character.eye_color}
+      </p>
+      <p>
+        <b>Homeworld:</b> {homeworldData.name}
+      </p>
     </div>
   );
 };
